9594b69c272a15d303685f492ed6a750e675d01e
Josef Kotoun <Jkotoun58@gmail.com>
2020-10-17 10:54:01 +0200

    Definition of error codes in error_codes.h

---
+++ src/error_codes.h
@@ -0,0 +1,30 @@
+#ifndef err_codes
+#define err_codes
+
+//lexical analysis 
+#define OK 0
+#define LEX_ERR 1
+
+//syntax analysis errors
+#define SYNTAX_ERR 2
+
+//semantic analysis errors
+#define UNDEFINED_VAR_ERR 3 //undefined function or variable
+#define VAR_REDEFINITION_ERR 3 //redefinition of function or variable
+
+#define DEFINED_VAR_DATATYPE_ERR 4 //data type error of new defined variable 
+#define DATATYPE_COMPATIBILITY_ERR 5 //data types incompatibility in expression
+
+#define ARGS_RETURNS_COUNT_ERR 6 //wrong count or type of arguments or returns in function
+
+#define OTHER_SEMANTIC_ERR 7
+#define DIVIDE_ZERO_ERR 9
+
+//internal compiler errors like malloc error etc.
+#define INTERNAL_COMPILER_ERR 99 
+
+
+
+
+#endif
+
