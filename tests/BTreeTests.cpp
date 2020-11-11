5791e4dfe9fb4e32a1305a407dcd3a1488239d3b
Josef Kotoun <Jkotoun58@gmail.com>
2020-11-11 21:47:43 +0100

    Changed symtable name datatype to dynamic string

--- tests/BTreeTests.cpp
+++ tests/BTreeTests.cpp
@@ -2,6 +2,7 @@
 extern "C" {
 #include "../src/headers/symtable.h"
 #include "../src/headers/error_codes.h"
+#include "../src/headers/str.h"
 }
 #include <gtest/gtest.h>
 
@@ -9,12 +10,40 @@ class BTreeTests : public ::testing::Test
 {
    protected:
     node *rootptr;  
+    string root;
+    string left;
+    string right;
+    string left_right;
+    string left_left;
+    string right_right;
+    string right_left;
    void SetUp() 
    {
        init(&rootptr);
+       strInit(&root);
+       strInit(&left);
+       strInit(&right);
+       strInit(&left_right);
+       strInit(&left_left);
+       strInit(&right_right);
+       strInit(&right_left);
+       strAddChar(&root, 'f');
+       strAddChar(&left, 'b');
+       strAddChar(&right, 'h');
+       strAddChar(&left_right, 'd');
+       strAddChar(&left_left, 'a');
+       strAddChar(&right_right, 'z');
+       strAddChar(&right_left, 'g');
    }
     void TearDown() 
    {
+       strFree(&root);
+       strFree(&left);
+       strFree(&right);
+       strFree(&left_right);
+       strFree(&left_left);
+       strFree(&right_right);
+       strFree(&right_left);
       dispose_tree(&rootptr);
    }
 };
@@ -23,82 +52,79 @@ class BTreeTests : public ::testing::Test
 TEST_F(BTreeTests, VarTreeTest)
 {
     //insert values
-    char root[] = "f";
-    char left[] = "b";
-    char right[] = "h";
-    char left_right[] = "d";
-    char left_left[] = "a";
-    char right_right[] = "z";
-    char right_left[] = "g";
-    EXPECT_EQ(insert_node_var(&rootptr,root, INT), OK);
-    EXPECT_EQ(insert_node_var(&rootptr,left,INT), OK);
-    EXPECT_EQ(insert_node_var(&rootptr, right, STRING), OK);
-    EXPECT_EQ(insert_node_var(&rootptr, right_left, STRING), OK);
-    EXPECT_EQ(insert_node_var(&rootptr, right_right, STRING), OK);
-    EXPECT_EQ(insert_node_var(&rootptr, left_left, STRING), OK);
-    EXPECT_EQ(insert_node_var(&rootptr, left_right, STRING), OK);
-    EXPECT_EQ(rootptr->name, root);
-    EXPECT_EQ(rootptr->l_ptr->name, left);
-    EXPECT_EQ(rootptr->r_ptr->name, right);
-    EXPECT_EQ(rootptr->l_ptr->l_ptr->name, left_left);
-    EXPECT_EQ(rootptr->l_ptr->r_ptr->name, left_right);
-    EXPECT_EQ(rootptr->r_ptr->r_ptr->name, right_right);
-    EXPECT_EQ(rootptr->r_ptr->l_ptr->name, right_left);
+    
+    EXPECT_EQ(insert_node_var(&rootptr,&root, INT), OK);
+    EXPECT_EQ(insert_node_var(&rootptr,&left,INT), OK);
+    EXPECT_EQ(insert_node_var(&rootptr, &right, STRING), OK);
+    EXPECT_EQ(insert_node_var(&rootptr, &right_left, STRING), OK);
+    EXPECT_EQ(insert_node_var(&rootptr, &right_right, STRING), OK);
+    EXPECT_EQ(insert_node_var(&rootptr, &left_left, STRING), OK);
+    EXPECT_EQ(insert_node_var(&rootptr, &left_right, STRING), OK);
+    EXPECT_EQ(strCmpString(rootptr->name, &root), 0);
+    EXPECT_EQ(strCmpString(rootptr->l_ptr->name, &left), 0);
+    EXPECT_EQ(strCmpString(rootptr->r_ptr->name, &right), 0);
+    EXPECT_EQ(strCmpString(rootptr->l_ptr->l_ptr->name, &left_left), 0);
+    EXPECT_EQ(strCmpString(rootptr->l_ptr->r_ptr->name, &left_right), 0);
+    EXPECT_EQ(strCmpString(rootptr->r_ptr->r_ptr->name, &right_right), 0);
+    EXPECT_EQ(strCmpString(rootptr->r_ptr->l_ptr->name, &right_left), 0);
 
     //search for values
     node* found = NULL;
-    char key1[] = "asdqwe";
-    EXPECT_FALSE(search(&rootptr, key1, &found));
+    string key1;
+    strInit(&key1);
+    strAddChar(&key1, 'a');
+    strAddChar(&key1, 'b');
+    EXPECT_FALSE(search(&rootptr, &key1, &found));
     EXPECT_EQ(found, nullptr);
     
-    char key2[] = "b";
-    EXPECT_TRUE(search(&rootptr, key2, &found));
-    EXPECT_EQ(strcmp(found->name,key2), 0);
+    strClear(&key1);
+    strAddChar(&key1, 'b');
+    EXPECT_TRUE(search(&rootptr, &key1, &found));
+    EXPECT_EQ(strCmpString(found->name,&key1), 0);
     EXPECT_EQ(found->type, var);
     EXPECT_EQ(((symbol_variable*)found->data)->var_type, INT);
 
-    char key3[] = "g";
-    EXPECT_TRUE(search(&rootptr, key3, &found));
-    EXPECT_EQ(strcmp(found->name,key3), 0);
+    strClear(&key1);
+    strAddChar(&key1, 'g');
+    EXPECT_TRUE(search(&rootptr, &key1, &found));
+    EXPECT_EQ(strCmpString(found->name,&key1), 0);
     EXPECT_EQ(found->type, var);
     EXPECT_EQ(((symbol_variable*)found->data)->var_type, STRING);
 
     //dispose tree test
     EXPECT_EQ(dispose_tree(&rootptr), OK);
     EXPECT_EQ(rootptr, nullptr);   
+    strFree(&key1);
 }
 
 
 TEST_F(BTreeTests, FuncTreeTest)
 {
-      //insert values
-    char root[] = "f";
-    char left[] = "b";
-    char right[] = "h";
-    char left_right[] = "d";
-    char left_left[] = "a";
-    char right_right[] = "z";
-    char right_left[] = "g";
+
     varType returns[] = {INT, FLOAT};
     varType params[] = {INT, STRING, FLOAT};
     varType returns2[] = {INT};
     varType params2[] = {STRING};
-    EXPECT_EQ(insert_node_func(&rootptr,root,2,returns,3,params,true), OK);
-    EXPECT_EQ(insert_node_func(&rootptr,left,1,returns2,1,params2,true), OK);
-    EXPECT_EQ(insert_node_func(&rootptr,right,0,NULL,0,NULL,true), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&root,2,returns,3,params,true), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&left,1,returns2,1,params2,true), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&right,0,NULL,0,NULL,true), OK);
 
-    EXPECT_EQ(insert_node_func(&rootptr,left_right,1,returns2,3,params,true), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&left_right,1,returns2,3,params,true), OK);
 
-    EXPECT_EQ(insert_node_func(&rootptr,left_left,0,NULL,1,params2,false), OK);
-    EXPECT_EQ(insert_node_func(&rootptr,right_right,1,returns2,1,params2,true), OK);
-    EXPECT_EQ(insert_node_func(&rootptr,right_left,2,returns,1,params2,true), OK);
-    char non_existing[] = "asdf";
+    EXPECT_EQ(insert_node_func(&rootptr,&left_left,0,NULL,1,params2,false), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&right_right,1,returns2,1,params2,true), OK);
+    EXPECT_EQ(insert_node_func(&rootptr,&right_left,2,returns,1,params2,true), OK);
+    string non_existing;
+    strInit(&non_existing);
+    strAddChar(&non_existing, 'a');
+    strAddChar(&non_existing, 's'); 
+    
     node* found = nullptr;
-    EXPECT_FALSE(search(&rootptr,non_existing, &found));
+    EXPECT_FALSE(search(&rootptr,&non_existing, &found));
     EXPECT_EQ(found, nullptr);
-
-    EXPECT_TRUE(search(&rootptr, left_right, &found));
-    EXPECT_EQ(strcmp(found->name, left_right), 0);
+    strFree(&non_existing);
+    EXPECT_TRUE(search(&rootptr, &left_right, &found));
+    EXPECT_EQ(strCmpString(found->name, &left_right), 0);
     EXPECT_EQ(found->type, func);
     symbol_function* data = (symbol_function*)found->data;
     EXPECT_TRUE(data->defined);
