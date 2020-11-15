6845e87f739626d31f19c461d971c64ccd3b995f
Jiří Vlasák <jirivlasak9@gmail.com>
2020-11-15 18:31:33 +0100

    AAAAAA

--- src/main.c
+++ src/main.c
@@ -4,11 +4,7 @@
 int main(int argc, char const* argv[])
 {
     //TESTING ONLY
-<<<<<<< HEAD
     // stdin = fopen("../tests/parserTestSources/expressions/expr2.go", "r");
-=======
-    stdin = fopen("../tests/parserTestSources/semantic/others/func_multiple_return_vars.go", "r");
->>>>>>> parser_semantic_anal
     parser_start();
     // fprintf(stderr, "SUCC\n");
     return 0;
